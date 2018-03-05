7829a8c99f15cc15a797cd8f6a31153188b13072
dyllanwli <lidiyaa03@gmail.com>
2018-03-05 14:15:43 +0800

    add new peer

---
+++ newOrg/first-network/anp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+
+function printHelp () {
+    echo "This script is used for spawning new peer"
+}
+
+
+export IMAGE_TAG=x86_64-1.1.0-rc1
+# set default image tag
+
+while getopts "h?m:c:t:d:f:s:l:i:" opt; do
+  case "$opt" in
+    h|\?)
+      printHelp
+      exit 0
+    i)  IMAGETAG=`uname -m`"-"$OPTARG
+    ;;
+  esac
+done
+
+
+echo "=====> Spawning new peer using x86_64-1.1.0-rc1"
+IMAGE_TAG=$IMAGETAG docker-compose -f docker-compose-new-peer.yaml up -d
+
+echo "===> checking whether the peer has been launched"
+docker ps 
+
+echo "===> Joinning mychannel"
+docker exec -it cli ./script/new-peer-org2.sh
---
+++ newOrg/first-network/scripts/new-peer-org2.sh
@@ -0,0 +1,9 @@
+
+
+export CHANNEL_NAME=mychannel
+CORE_PEER_LOCALMSPID="Org2MSP"
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+CORE_PEER_ADDRESS=peer2.org2.example.com:7051
+
+peer channel join -b mychannel.block
