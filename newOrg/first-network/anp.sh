8a9c1901bc9ec5377dfd458cec062ccbde911f8a
dyllanwli <lidiyaa03@gmail.com>
2018-03-05 14:24:44 +0800

    add s

--- newOrg/first-network/anp.sh
+++ newOrg/first-network/anp.sh
@@ -15,17 +15,17 @@ while getopts "h?m:c:t:d:f:s:l:i:" opt; do
       printHelp
       exit 0
     ;;
-    i)  IMAGETAG=`uname -m`"-"$OPTARG
+    i)  IMAGE_TAG=`uname -m`"-"$OPTARG
     ;;
   esac
 done
 
 
 echo "=====> Spawning new peer using $IMAGE_TAG"
-IMAGE_TAG=$IMAGETAG docker-compose -f docker-compose-new-peer.yaml up -d
+IMAGE_TAG=$IMAGE_TAG docker-compose -f docker-compose-new-peer.yaml up -d
 
 echo "===> checking whether the peer has been launched"
 docker ps 
 
 echo "===> Joinning mychannel"
-docker exec -it cli ./script/new-peer-org2.sh
+docker exec -it cli ./scripts/new-peer-org2.sh
