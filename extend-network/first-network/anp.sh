c99fe613e37794df5ecb609d7f6e5cdec280dfdc
dyllanwli <lidiyaa03@gmail.com>
2018-03-06 09:13:31 +0800

    fix bug

--- extend-network/first-network/anp.sh
+++ extend-network/first-network/anp.sh
@@ -32,13 +32,19 @@ echo "===> checking whether the peer has been launched"
 docker ps 
 
 echo "==> chmod those script incase get permission denied"
-chmod +x ./scripts/new-peer-join.sh
-chmod +x ./scripts/new-peer-chaincode.sh
+chmod +x ./scripts/new-peer-1.sh
+chmod +x ./scripts/new-peer-2.sh
+chmod +x ./scripts/new-peer-3.sh
 
 echo "===> Joinning mychannel"
-docker exec -it cli ./scripts/new-peer-join.sh
+docker exec -it cli ./scripts/new-peer-1.sh
 
-echo "===> Chaincode is not finished"
-docker exec -it cli ./scripts/new-peer-chaincode.sh
+echo "===> Installing chaincode"
+docker exec -it cli ./scripts/new-peer-2.sh
 # peer chaincode install -n mycc -v 2.0 -l golang -p github.com/chaincode/chaincode_example02/go/
 # peer chaincode upgrade -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc -v 2.0 -c '{"Args":["init","a","90","b","210"]}' -P 'OR ('\''Org1MSP.peer'\'','\''Org2MSP.peer'\'')'
+
+echo "===> Instantiating chaincode"
+docker exec -it cli ./scripts/new-peer-3.sh
+
+echo "===> If no error, all done."
--- extend-network/first-network/scripts/new-peer-2.sh
+++ extend-network/first-network/scripts/new-peer-2.sh
@@ -13,5 +13,4 @@ CORE_PEER_ID=cli
 CORE_LOGGING_LEVEL=INFO
 CORE_PEER_ADDRESS=peer2.org2.example.com:7051
 
-peer chaincode install -n mycc -v 2.0 -l golang -p github.com/chaincode/chaincode_example02/go/
 peer chaincode upgrade -o orderer.example.com:7050 --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -C mychannel -n mycc -v 2.0 -c '{"Args":["init","a","90","b","210"]}' -P 'OR ('\''Org1MSP.peer'\'','\''Org2MSP.peer'\'')'
