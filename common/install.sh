4018c893e3e0c7609b0bd322a215429d54b3e376
dyllanwli <lidiyaa03@gmail.com>
2018-03-06 11:46:55 +0800

    add script to install golang

---
+++ common/install.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -e
+CURRENT="$(dirname $(readlink -f ${BASH_SOURCE}))"
+
+fcn=$1
+
+systemProfile="/etc/profile"
+remain_params=""
+for ((i = 2; i <= ${#}; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
+
+function golang() {
+    goTar=go1.9.2.linux-amd64.tar.gz
+    if go version >/dev/null;then
+        echo ... to overwrite exiting golang at GOROOT: $(go env GOROOT)
+        sudo rm -rf $(go env GOROOT)
+    fi
+	# install golang
+	if docker version >/dev/null ;then
+        goTar=$(docker version | grep -m1 go| awk '{print($3)}').linux-amd64.tar.gz
+        echo ... to use docker inline go version :[${goTar}]
+    fi
+	wget https://redirector.gvt1.com/edgedl/go/${goTar}
+	sudo tar -C /usr/local -xzf ${goTar}
+	# write path to 'go' command
+	if ! grep "/usr/local/go/bin" $systemProfile; then
+		echo "export PATH=\$PATH:/usr/local/go/bin" | sudo tee -a $systemProfile
+	fi
+	# write path to $GOPATH/bin
+	GOPATH=$(go env GOPATH)
+	if ! grep "$GOPATH/bin" $systemProfile; then
+		echo "export PATH=\$PATH:$GOPATH/bin" | sudo tee -a $systemProfile
+	fi
+	# delete install pack
+	rm -f ${goTar}
+}
+function golang-uninstall() {
+	# TODO  To remove an existing Go installation from your system delete the go directory. 
+    # This is usually /usr/local/go under Linux, Mac OS X, and FreeBSD
+	# You should also remove the Go bin directory from your PATH environment variable. 
+    # Under Linux and FreeBSD you should edit /etc/profile or $HOME/.profile. 
+    # If you installed Go with the Mac OS X package then you should remove the /etc/paths.d/go file. 
+}
+
+
+function nodejs(){
+	echo "===== Install nvm ====="
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+	echo "===== Add those below to you ~/.bashrc. then run source ===== "
+	echo "
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+	"
+	command -v nvm
+}
+
+function govendor(){
+    go get -u github.com/kardianos/govendor
+}
+if [ -n "$fcn" ]; then
+	$fcn
+	nodejs
+else
+	"Nothing to do. end. "
+fi
---
+++ install-prerequisite.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# Use on ECS to initialize the envrionment.
+
+
+# No need to install docker
+# sudo yum remove docker \
+#                 docker-client \
+#                 docker-client-latest \
+#                 docker-common \
+#                 docker-latest \
+#                 docker-latest-logrotate \
+#                 docker-logrotate \
+#                 docker-selinux \
+#                 docker-engine-selinux \
+#                 docker-engine
+
+# sudo yum install -y yum-utils \
+#       device-mapper-persistent-data \
+#       lvm2
+# sudo yum-config-manager \
+#     --add-repo \
+#     https://download.docker.com/linux/centos/docker-ce.repo
+
+# sudo yum-config-manager --disable docker-ce-edge
+
+
+set -e
+CURRENT=pwd
+fcn=$1
+
+# install some toolkit
+yum install git lsof jq tree curl
+
+function cn() {
+	if [ ! -f "$CURRENT/common/install.sh" ]; then
+		gitSync
+	fi
+	$CURRENT/common/install.sh cn
+	sudo apt -qq install -y moreutils
+	npm install
+}
+
+if [ -n "$fcn" ]; then
+	$fcn
+else
+	$CURRENT/common/install.sh
+	# write to config: jq do not support in-place editing, use moreutils:sponge
+	yum -qq install -y moreutils
+	if ! go version; then
+		$CURRENT/common/install.sh golang
+	fi
+fi
